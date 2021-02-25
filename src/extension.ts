19ab247d5dfbfa54627ad1819247d6b70ee81577
taoxinhua <taoxinhua@xzintl.com>
2021-02-25 10:35:17 +0800

    首次提交

---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "xz-generate-id" is now active!',
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'xz-generate-id.helloWorld',
+    () => {
+      const { activeTextEditor } = vscode.window;
+
+      if (!activeTextEditor) {
+        return; // no editor
+      }
+      // The code you place here will be executed every time your command is executed
+      activeTextEditor.edit(editBuilder => {
+        editBuilder.replace(
+          activeTextEditor.selection.active,
+          Math.random().toString(36).slice(2, 10),
+        );
+      });
+    },
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
