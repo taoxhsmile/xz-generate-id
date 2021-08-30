dd89e8844ebc8fe32568f690e2f7b760ff95e972
taoxinhua <taoxinhua@xzintl.com>
2021-08-30 14:54:34 +0800

    增加移除未使用的定义的功能

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const extensionLanguages = [
+  "javascript",
+  "javascriptreact",
+  "typescript",
+  "typescriptreact",
+];
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as t from "@babel/types";
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import generate from "@babel/generator";
+import { isValidDocumentLanguage } from "./utils";
+import { extensionLanguages } from "./constants";
+
+console.log("extensionLanguages=", extensionLanguages);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,9 +22,8 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerTextEditorCommand(
-    "xz-generate-id.generate",
-    () => {
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand("xz-generate-id.generate", () => {
       const { activeTextEditor } = vscode.window;
       if (!activeTextEditor) {
         return;
@@ -29,10 +36,83 @@ export function activate(context: vscode.ExtensionContext) {
           );
         });
       });
-    }
+    })
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand(
+      "xz-generate-id.rm-js-nouse",
+      () => {
+        const { activeTextEditor } = vscode.window;
+        if (!activeTextEditor) {
+          return;
+        }
+
+        if (!isValidDocumentLanguage(activeTextEditor.document)) {
+          return;
+        }
+
+        const code = activeTextEditor.document.getText();
+
+        const ast = parser.parse(code, {
+          sourceType: "module",
+          plugins: ["typescript", "jsx"],
+        });
+
+        traverse(ast, {
+          VariableDeclaration(path) {
+            const { node } = path;
+            const { declarations } = node;
+
+            node.declarations = declarations.filter((declaration) => {
+              const { id } = declaration;
+              // scope.getBinding(name) 获取当前所有绑定
+              // scope.getBinding(name).referenced 绑定是否被引用
+              // scope.getBinding(name).constantViolations 获取当前所有绑定修改
+              // scope.getBinding(name).referencePaths  获取当前所有绑定路径
+              const binding = path.scope.getBinding((id as any).name);
+              return !!binding?.referenced;
+            });
+
+            if (node.declarations.length === 0) {
+              path.remove();
+            }
+          },
+          ImportDeclaration(path) {
+            const { node } = path;
+            const { specifiers } = node;
+            if (!specifiers.length) return;
+            node.specifiers = specifiers.filter((specifier) => {
+              const { local } = specifier;
+              const binding = path.scope.getBinding(local.name);
+              return !!binding?.referenced;
+            });
+            if (node.specifiers.length === 0) {
+              path.remove();
+            }
+          },
+          FunctionDeclaration(path) {
+            const { node } = path;
+            const { id } = node;
+            const binding = path.scope.getBinding(id!.name);
+            if (!binding?.referenced) {
+              path.remove();
+            }
+          },
+        });
+
+        activeTextEditor.edit((editBuilder) => {
+          editBuilder.replace(
+            new vscode.Range(
+              new vscode.Position(0, 0),
+              new vscode.Position(activeTextEditor.document.lineCount + 1, 0)
+            ),
+            generate(ast).code
+          );
+        });
+      }
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
